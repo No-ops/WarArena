792ef92adf6a3d237e3a29cd08ab25960c57bd39
Anders Eklöf <anderse77@gmail.com>
2016-02-18 15:37:20 +0100

    Commit initiator

---
+++ WarArena/Initiator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using WarArena.Models;
+
+namespace WarArena
+{
+    static class Initiator
+    {
+        public static MapperConfiguration configuration;
+
+        public static void AutoMapperConfig()
+        {
+            configuration = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Player, PlayerModel>();
+                config.CreateMap<PlayerModel, Player>();
+            });
+            
+        }
+    }
+}
