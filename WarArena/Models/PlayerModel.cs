80b3130dac63fe7605bbc69b1bd653037d837f1e
Anders Eklöf <anderse77@gmail.com>
2016-02-18 14:24:42 +0100

    Updated PlayerModel v1.3

--- WarArena/Models/PlayerModel.cs
+++ WarArena/Models/PlayerModel.cs
@@ -16,7 +16,5 @@ namespace WarArena.Models
         public int Gold { get; set; }
         public int Health { get; set; }
         public bool IsDead { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
     }
 }
