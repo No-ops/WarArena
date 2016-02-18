00fd4d55b4636363c14ee4c1527657ebbca7e8b4
Anders Eklöf <anderse77@gmail.com>
2016-02-18 14:16:49 +0100

    Uppdaterade PlayerModel

--- WarArena/Models/PlayerModel.cs
+++ WarArena/Models/PlayerModel.cs
@@ -8,6 +8,7 @@ namespace WarArena.Models
 {
     public class PlayerModel
     {
+        public int Id { get; set; }
         public int PlayerId { get; set; }
         public string Name { get; set; }
         public int Attack { get; set; }
@@ -16,6 +17,5 @@ namespace WarArena.Models
         public bool IsDead { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
-        public string PlayerColor { get; set; }
     }
 }
