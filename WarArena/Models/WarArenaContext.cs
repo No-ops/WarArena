1add6ceb62dbfd0a3f10139e95aff67e50a15f64
No-ops <c.fjellstrom@gmail.com>
2016-02-18 13:11:54 +0100

    Added Entity Framework

---
+++ WarArena/Models/WarArenaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarArena.Models
+{
+    class WarArenaContext : DbContext
+    {
+        public DbSet<Thing> Things { get; set; }
+    }
+}
