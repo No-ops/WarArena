46571d626916c1c3f87ab3b013a3c046cdfe4d6e
Anders Eklöf <anderse77@gmail.com>
2016-02-18 13:56:10 +0100

    Skapade PlayerModel.

---
+++ WarArena/Models/PlayerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarArena.Models
+{
+    public class PlayerModel
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Gold { get; set; }
+        public int Health { get; set; }
+        public bool IsDead { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string PlayerColor { get; set; }
+    }
+}
