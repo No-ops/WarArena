2df91ac1153ee11232adafc89734330bd0ab659e
No-ops <c.fjellstrom@gmail.com>
2016-02-25 11:46:43 +0100

    Response class

---
+++ WarArena/Response.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarArena
+{
+    class Response
+    {
+        public enum MessageType
+        {
+            NEWPLAYER, REMOVEPLAYER, UPDATEPLAYER, UPDATETILE, YOURTURN, DENIED, 
+        }
+        public string StringParam { get; set; }
+        public MessageType ResponseType { get; set; }
+        public int IdParam { get; set; }
+        public Socket Socket { get; set; }
+
+        private static void SendString(string message, Socket socket)
+        {
+            
+        }
+
+        public static void SendNewPlayerResponses(List<Client> clients, WorldMap world, int id)
+        {
+            foreach (var client in clients)
+            {
+                if (client.Player.PlayerId == id)
+                {
+                    var worldBuilder = new StringBuilder("WAP/1.0 SENDSTATE");
+                    AddWorld(clients, world, worldBuilder);
+                    SendString(worldBuilder.ToString(), client.Socket);
+                }
+                else
+                {
+                    SendString($"WAP/1.0 NEWPLAYER {client.Player.Name},{client.Player}", client.Socket);
+                }
+            }
+        }
+
+        private static void AddWorld(List<Client> clients, WorldMap world, StringBuilder stringBuilder)
+        {
+            foreach (var client in clients)
+            {
+                var player = client.Player;
+                stringBuilder.Append(
+                    $" Pl,{player.Name},{player.PlayerId},{player.Coordinates.X},{player.Coordinates.Y},{player.Health},{player.Gold}");
+            }
+
+            foreach (var tile in world.GameMap)
+            {
+                if(tile.HasGold)
+                    stringBuilder.Append($" G,{tile.X},{tile.Y},{tile.Gold}");
+
+                if(tile.HasHealth)
+                    stringBuilder.Append($" P,{tile.X},{tile.Y},{tile.Health}");
+            }
+        }
+
+        public static void SendDeniedResponse(List<Client> clients, Socket socket, string stringParam)
+        {
+            SendString($"WAP/1.0 DENIED {stringParam}", socket);
+        }
+
+        public static void SendRemovePlayerResponse(List<Client> clients, int id)
+        {
+            foreach (var client in clients)
+            {
+                SendString($"WAP/1.0 REMOVEPLAYER {id}", client.Socket);
+            }
+        }
+
+        public static void SendUpdateTile(List<Client> clients, string stringParam)
+        {
+            foreach (var client in clients)
+            {
+                SendString($"WAP/1.0 UPDATETILE {stringParam}", client.Socket);
+            }
+        }
+
+        public static void SendYourTurn(List<Client> clients, int id)
+        {
+            var client = clients.Single(c => c.Player.PlayerId == id);
+            SendString($"WAP/1.0 YOURTURN {id}", client.Socket);
+
+        }
+
+        public static void SendUpdatePlayer(List<Client> clients, int id)
+        {
+            var updatedPlayer = clients.Single(c => c.Player.PlayerId == id);
+            foreach (var client in clients)
+            {
+                SendString($"WAP/1.0 UPDATEPLAYER {updatedPlayer}", client.Socket);
+            }
+        }
+    }
+}
