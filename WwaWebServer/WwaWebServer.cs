6f9a2483a0bb6c82cd82e75291fbdafc42084cbf
No-ops <c.fjellstrom@gmail.com>
2016-03-08 10:39:40 +0100

    Weird wwawebserver commit

---
+++ WwaWebServer/WwaWebServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WwaWebServer
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    class WwaWebServer : IWwaWebServer
+    {
+        public Statistics GetStats()
+        {
+            return new Statistics();
+        }
+    }
+}
